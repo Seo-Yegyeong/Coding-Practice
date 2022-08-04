838ecb7626d5ebb3450a4248410e470a0b3cbf96
Lab <seoye6618@gmail.com>
2022-08-04 18:22:43 +0900

    test

---
+++ basics/baekjoon_10926.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char input[] = "";
+
+    scanf("%s", input);
+    strlwr(input);
+    //strcat(input, "??!");
+
+    printf(input);
+
+    return 0;
+}
